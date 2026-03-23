a2f41344ed23491968a53f42235bd0022a16b2a5
David Radcliffe <dradcliffe@gmail.com>
2026-03-23 07:21:45 -0500

    daily updates

--- src/oeispy/A002/A002473.py
+++ src/oeispy/A002/A002473.py
@@ -6,13 +6,16 @@ import heapq
 from itertools import islice
 from sympy import primerange
 def A002473gen(p=7): # generate all p-smooth terms
-    v, oldv, h, psmooth_primes, = 1, 0, [1], list(primerange(1, p+1))
+    v, oldv, h, psmooth_primes, heaps = 1, 0, [1], list(primerange(1, p+1)), {1}
     while True:
         v = heapq.heappop(h)
         if v != oldv:
             yield v
             oldv = v
             for p in psmooth_primes:
-                heapq.heappush(h, v*p)
-print(list(islice(A002473gen(), 65))) # _Michael S. Branicky_, Nov 19 2022
+                if (m:=v*p) not in heaps:
+                    heapq.heappush(h, m)
+                    heaps.add(m)
+        heaps.discard(v)
+print(list(islice(A002473gen(), 65))) # _Michael S. Branicky_, Nov 19 2022, edited Mar 22 2026
 
